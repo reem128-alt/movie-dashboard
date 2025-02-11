1d7057286c382bf1d6a6d5482d7d3e1ce33b7f19
reem128-alt <122190881+reem128-alt@users.noreply.github.com>
2025-02-12 00:11:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 dbConnect();
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://movie-dash-front.vercel.app"] 
+    origin: "http://localhost:5173","https://movie-dash-front.vercel.app" 
     credentials: true,
   })
 );
